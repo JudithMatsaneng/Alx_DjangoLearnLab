867b3431fc6a487bf0eb28cb089a9299a473fb73
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-07-17 08:35:34 +0200

    updated files

--- LibraryProject/relationship_app/query_samples.py
+++ LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,12 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from .models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
-def books_by_author(author_name):
+def get_books_by_author(author_name):
     return Book.objects.filter(author__name=author_name)
 
-# List all books in a library
-def books_in_library(library_name):
+def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-# Retrieve the librarian for a library
-def librarian_for_library(library_name):
+def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
