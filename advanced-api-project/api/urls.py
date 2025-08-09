c2fe87a3960c7042f8b5c7fbf52f84f7188df75b
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-08-09 14:29:33 +0200

    Set up a new project -advanced-api-project

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import AuthorList, BookList
+
+urlpatterns = [
+    path('authors/', AuthorList.as_view(), name='author-list'),
+    path('books/', BookList.as_view(), name='book-list'),
+]
+
