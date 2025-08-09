749332561d1f5b611c5f5bbab972f7faeacbefae
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-08-09 10:54:58 +0200

    Task 1 updates-Building API endpoints

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -8,3 +8,10 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
