4f24bfbc4f9d0fcb37824db8696eefa2757863ff
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-08-17 19:51:52 +0200

    updated and added tags properly

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 from django.utils import timezone
 from django.urls import reverse
 
@@ -11,6 +12,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField("Tag", blank=True, related_name="posts")
+    tags = TaggableManager()
+
 
     def __str__(self):
         return self.title
