c4417b277595af3bf1c16119e02dbea79d4e3e38
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-08-21 00:00:29 +0200

    Updated Models.py to include timestamp

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 
 class Notification(models.Model):
     recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications")
@@ -10,7 +12,8 @@ class Notification(models.Model):
     target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True)
     target_object_id = models.PositiveIntegerField(null=True, blank=True)
     target = GenericForeignKey("target_content_type", "target_object_id")
-    created_at = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)  
+    created_at = models.DateTimeField(default=timezone.now)
     read = models.BooleanField(default=False)
 
     class Meta:
