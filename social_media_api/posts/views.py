699a4a67fb73949df895fc6f50c1c09f7b503a03
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-08-20 18:12:57 +0200

    updated views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-# Create your views here.
+# Custom permission: only allow authors to edit/delete their own content
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # SAFE methods (GET, HEAD, OPTIONS) are allowed for everyone
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Only the author can update/delete
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
